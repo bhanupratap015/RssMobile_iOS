a35616821334c155203e9e2eef8cfa4aae341d9a
bhanupratap015 <bhanupratap015@gmail.com>
2016-07-01 00:35:30 +0530

    Initial Commit

---
+++ RssMobile_iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RssMobile_iOS
+//
+//  Created by Bhanu Pratap on 01/07/16.
+//  Copyright © 2016 Bhanu Pratap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
